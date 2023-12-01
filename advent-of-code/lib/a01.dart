f25684fda7b083f0e65a6b47608ee3ea006fcb55
Lucas Granberg <lucgr483@student.liu.se>
2023-12-01 20:33:21 +0100

    Small edit

--- advent-of-code/lib/a01.dart
+++ advent-of-code/lib/a01.dart
@@ -54,6 +54,7 @@ Future<List<String>> getNumbers2() async {
   }
 
   final List<String> result = [];
+  final List<String> newLines = [];
   final file = File('res/input1.txt');
   Stream<String> lines =
       file.openRead().transform(utf8.decoder).transform(LineSplitter());
@@ -62,13 +63,15 @@ Future<List<String>> getNumbers2() async {
       for (int i in wordToNum.keys) {
         int c = 0;
         for (Match m in wordToNum[i]!.allMatches(line)) {
-          line = line.substring(0, m.start + c) +
+          line = line.substring(0, m.start + c * numberStrings[i - 1].length) +
+              numberStrings[i - 1] +
               i.toString() +
-              line.substring(m.start + c, line.length);
+              line.substring(
+                  m.start + c * numberStrings[i - 1].length, line.length);
           c++;
         }
       }
-
+      newLines.add(line);
       final splitLine = line.split('');
       isNum(s) => int.tryParse(s) != null;
       var first = splitLine.firstWhere((s) => isNum(s));
